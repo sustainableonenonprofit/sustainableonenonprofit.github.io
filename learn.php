318176553151df2e6eb2c9f831e822c7dcc83c9a
Kelly <lykelly2014@icloud.com>
2019-11-30 12:28:58 -0500

    show contents available for learn

--- learn.php
+++ learn.php
@@ -24,7 +24,7 @@
     <section>
     <div class="media">
       <a href="epa_article">
-      <img src="assets/learn_assets/tree_sea.jpg">
+      <img src="assets/learn_assets/tree_sea.jpg" alt="tree_sea.jpg photo">
       <div class="text">
         <p class="topic article">ARTICLE<br>___________</p>
         <p class="name">Brief History of the EPA</p>
@@ -33,16 +33,26 @@
     </div>
     <div class="media">
       <a href="foodwaste_article">
-      <img src="assets/learn_assets/lake_mist.png">
+      <img src="assets/learn_assets/tropical_trees.png" alt="tropical_trees.png image">
       <div class="text">
         <p class="topic article">ARTICLE<br>___________</p>
         <p class="name">A Solution to Food Waste</p>
       </div>
       </a>
     </div>
+
+
+    <div class="media">
+      <img src="assets/learn_assets/mtn_mist.png" alt="mountain mist image">
+      <div class="text">
+        <p class="name">More Coming Soon!</p>
+      </div>
+    </div>
+    <!--
+
     <div class="media">
       <a href="#">
-      <img src="assets/learn_assets/tropical_trees.png">
+      <img src="assets/learn_assets/lake_mist.png" alt="lake_mist.png photo">
       <div class="text">
         <p class="topic article">ARTICLE<br>___________</p>
         <p class="name">Matcha Mango Tea</p>
@@ -52,7 +62,7 @@
 
     <div class="media">
       <a href="#">
-      <img src="assets/learn_assets/sit_grass.jpg">
+      <img src="assets/learn_assets/sit_grass.jpg" alt="grass image">
       <div class="text">
         <p class="topic infographic">INFOGRAPHIC<br>___________</p>
         <p class="name">Jellyfish and Stuff</p>
@@ -62,7 +72,7 @@
 
     <div class="media">
       <a href="#">
-      <img src="assets/learn_assets/road_mtns.jpg">
+      <img src="assets/learn_assets/road_mtns.jpg" alt="mountains image">
       <div class="text">
         <p class="topic infographic">INFOGRAPHIC<br>___________</p>
         <p class="name">Apple Cinnamon Toast Crunch</p>
@@ -72,24 +82,14 @@
 
     <div class="media">
       <a href="#">
-      <img src="assets/learn_assets/mtn_mist.png">
-      <div class="text">
-        <p class="topic article">ARTICLE<br>___________</p>
-        <p class="name">Coconut Clusters</p>
-      </div>
-      </a>
-    </div>
-
-    <div class="media">
-      <a href="#">
-      <img src="assets/learn_assets/stars_reflecting.jpg">
+      <img src="assets/learn_assets/stars_reflecting.jpg" alt="stars reflecting image">
       <div class="text">
         <p class="topic online-resource">ONLINE RESOURCE<br>___________</p>
         <p class="name">Organic Passion Fruit</p>
       </div>
       </a>
     </div>
-
+-->
   </section>
 
     <?php include 'footer.php' ?>
